c7c18f7d15a78d2ee1f78e90cd8304995562321f
Arnaud474 <arnaud.girardin444@gmail.com>
2017-10-26 16:45:13 -0400

    Including drawer_layout in another layout

--- app/src/main/java/com/example/am90700/test/activities/BaseDrawerActivity.java
+++ app/src/main/java/com/example/am90700/test/activities/BaseDrawerActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -24,7 +23,6 @@ public abstract class BaseDrawerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
 
         sections = getResources().getStringArray(R.array.sections);
         drawerLayout = (DrawerLayout) findViewById(R.id.menu_drawer);
--- app/src/main/java/com/example/am90700/test/activities/MainActivity.java
+++ app/src/main/java/com/example/am90700/test/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.am90700.test.activities;
 
 import android.os.Bundle;
 
+import com.example.am90700.test.R;
+
 /**
  * Created by am90700 on 17-10-26.
  */
@@ -10,6 +12,7 @@ public class MainActivity extends BaseDrawerActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.search_layout);
         super.onCreate(savedInstanceState);
     }
 }
